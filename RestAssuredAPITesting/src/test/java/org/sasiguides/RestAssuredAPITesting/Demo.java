5fe84f0a851eb6ae22d75063876ed4a8fba0a87d
SasikiranKakara <55825519+sasikirankakara001@users.noreply.github.com>
2023-09-16 03:04:40 +0530

    initial commit

---
+++ RestAssuredAPITesting/src/test/java/org/sasiguides/RestAssuredAPITesting/Demo.java
@@ -0,0 +1,26 @@
+package org.sasiguides.RestAssuredAPITesting;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class Demo {
+	@Test
+	public void performPostFunction() throws Exception {
+		RestAssured.baseURI = "https://rahulshettyacademy.com";
+		String res = given().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+				.body("{\r\n" + "  \"location\": {\r\n" + "    \"lat\": -38.383494,\r\n" + "    \"lng\": 33.427362\r\n"
+						+ "  },\r\n" + "  \"accuracy\": 50,\r\n" + "  \"name\": \"Frontline house\",\r\n"
+						+ "  \"phone_number\": \"(+91) 983 893 3937\",\r\n"
+						+ "  \"address\": \"29, side layout, cohen 09\",\r\n" + "  \"types\": [\r\n"
+						+ "    \"shoe park\",\r\n" + "    \"shop\"\r\n" + "  ],\r\n"
+						+ "  \"website\": \"http://google.com\",\r\n" + "  \"language\": \"French-IN\"\r\n" + "}\r\n"
+						+ "")
+				.when().post("/maps/api/place/add/json").then().log().all().extract().response().asString();
+		System.out.println(res);
+
+	}
+
+}
